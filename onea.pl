30bb7cba0e61cf86261f92185b3dc7ec3fc22b4f
jialiang zhang <jialiangz.zhang@mail.utoronto.ca>
2019-12-03 19:59:54 -0500

    modified onea

--- onea.pl
+++ onea.pl
@@ -1,26 +1,81 @@
 bot sub [s, lexical, phrase].
-    phrase sub [np, vp].
-    lexical sub [n, v, det].
+    phrase sub [vpsg, vppl, pp, npsg, nppl, np].
+    lexical sub [nprp, npl, vpl, vsg, det, nsg, npl, p].
 
-dog ---> n.
-cat ---> n.
+Fido ---> nprp.
+biscuits ---> npl.
+feed ---> vpl.
+feeds ---> vsg.
 the ---> det.
-likes ---> v.
-record ---> n.
-record ---> v.
+dog ---> nsg.
+puppies ---> npl.
+with ---> p.
 
 
-det_n__np rule
-    np ===>
-    cat> det,
-    cat> n.
-
-v_np__vp rule
-    vp ===>
-    cat> v,
+vsg_np__vpsg rule
+    vpsg ===>
+    cat> vsg,
     cat> np.
 
-np_vp__s rule
+vpl_np__vppl rule
+    vppl ===>
+    cat> vpl,
+    cat> np.
+
+p_np__pp rule
+    pp ===>
+    cat> p,
+    cat> np.
+
+nprp__npsg rule
+    npsg ===>
+    cat> nprp.
+
+det_nsg__npsg rule
+    npsg ===>
+    cat> det,
+    cat> nsg.
+
+det_nsg_pp__npsg rule
+    npsg ===>
+    cat> det,
+    cat> nsg,
+    cat> pp.
+
+det_npl__nppl rule
+    nppl ===>
+    cat> det,
+    cat> npl.
+
+det_npl_pp__nppl rule
+    nppl ===>
+    cat> det,
+    cat> npl,
+    cat> pp.
+
+npl__nppl rule
+    nppl ===>
+    cat> npl.
+
+npl_pp__nppl rule
+    nppl ===>
+    cat> npl,
+    cat> pp.
+
+npsg__np rule
+    np ===>
+    cat> npsg.
+
+nppl__np rule
+    np ===>
+    cat> nppl.
+
+npsg_vpsg__s rule
     s ===>
-    cat> np,
-    cat> vp.
+    cat> npsg,
+    cat> vpsg.
+
+nppl_vppl__s rule
+    s ===>
+    cat> nppl,
+    cat> vppl.
