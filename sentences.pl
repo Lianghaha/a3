421f6324c8c650d5b2618c81e67d55cd53e9d341
张家良 <jialiang0601@gmail.com>
2019-12-04 11:57:50 -0500

    test sentences

--- sentences.pl
+++ sentences.pl
@@ -3,3 +3,16 @@
 test_sent([nadia,won,an,elephant]).
 test_sent([i,could,have,demanded,a,rutabaga]).
 test_sent([autopoiesis,always,reminded,her,of],fails).
+
+good:
+rec[fido,feeds,the,dog,with,biscuits]
+rec[fido,feeds,the,dog,with,the,biscuits]
+rec[biscuits,feed,the,dog,with,biscuits]
+rec[the,dog,feeds,the,dog,with,biscuits]
+
+
+wrong:
+rec[fido,feed,the,dog,with,biscuits]
+rec[fido,feed,dog,with,biscuits]
+rec[dog,feed,the,dog,with,biscuits]
+rec[the,fido,feeds,the,dog,with,biscuits]
